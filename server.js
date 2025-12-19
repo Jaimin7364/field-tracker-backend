81286d50812c64485d9268f310bdb89d24512041
Jaimin Raval <163107078+Jaimin7364@users.noreply.github.com>
2025-12-19 20:12:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use(cors({
 app.use(express.json({ limit: "50mb" }));
 
 // === MONGO SETUP ===
-const MONGO_URI = "mongodb+srv://techtalk736:IxMykzec4BhXmUGc@cluster.gx78d6p.mongodb.net/field-tracker";
+const MONGO_URI = "Mongo_URI";
 mongoose.connect(MONGO_URI)
   .then(() => console.log("✅ MongoDB Connected"))
   .catch(err => console.error("❌ MongoDB Connection Error:", err));
